e6db00ed3ace53a7d77b9ff3874a611bde2dd0b0
ZampaglioneGaston <83976674+ZampaglioneGaston@users.noreply.github.com>
2021-05-10 18:01:15 -0300

    Programa V1

---
+++ cypress/integration/search.js
@@ -0,0 +1,48 @@
+
+/// <reference types="cypress"/>
+
+describe('DeliveryPizza', function(){
+
+    it('PedidosYa', function(){
+        cy.visit('/');                                                    //Ingreso a pedidos ya Argentina.
+
+        cy.fixture('Direction').then((variables) => {                    //Ingreso direccion, ciudad, opcion.
+            cy.get(variables.Country).click();                       
+            cy.get(variables.City).click();  
+            cy.city('[data-option-array-index="121"]');
+            cy.get(variables.Adress).type('Duarte Quiros 10')
+            cy.get(variables.Option).type('Pizza')
+        })
+
+        cy.search('#search')
+        cy.wait(1000);                                                      //Delay para que carge el mapa
+        cy.ConfirmMap('#confirm');                                          // Confirmo Mapa 
+
+        cy.fixture('Direction').then((variables) => {                       //Ingreso direccion, ciudad, opcion.
+            cy.get(variables.Sort).click();                                 //Menu de ordenamiento
+        })
+        cy.sort('[value="rating"] > a');                                    // Ordeno por puntuacion
+        cy.restaurant('[data-url="https://www.pedidosya.com.ar/restaurantes/cordoba/grido-helados-3122-velez-sarsfield-ii-av-velez-sarsfield-272-menu"] > .restaurantData > .infoFooter > .callToAction > .button');
+
+        cy.item('[data-id="23570597"] > b');                                // Item > pizza
+        cy.wait(1000);
+        cy.fixture('Direction').then((variables) =>{
+            cy.get(variables.OptionalText).type('Que venga caliente pls')   //Opcional en pedido
+        })
+            
+        cy.wait(1000);
+        cy.get('#footerOpen > .button').click();                            //Boton agregar al pedido
+        cy.wait(1000);
+
+        cy.fixture('Direction').then((variables) => {
+            cy.get(variables.Adress).type('Duarte Quiros 10', {force: true}) //Force true > elemento invisible
+
+        });   
+
+        cy.search('#search');                             //Force true > elemento invisible
+        cy.wait(3000);                                                      //Delay para que carge el mapa
+        cy.ConfirmMap('#confirm');                                          // Confirmo Mapa                                                            
+
+    })
+})
+
---
+++ cypress/support/commands.js
@@ -0,0 +1,41 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+Cypress.Commands.add('ConfirmMap', (element) => {cy.get(element).click({force: true})}) 
+
+Cypress.Commands.add('city', (element) => {cy.get(element).click({force: true})}) 
+
+Cypress.Commands.add('search', (element) => {cy.get(element).click({force: true})}) 
+
+Cypress.Commands.add('sort', (element) => {cy.get(element).click({force: true})}) 
+
+Cypress.Commands.add('restaurant', (element) => {cy.get(element).click({force: true})}) 
+
+Cypress.Commands.add('item', (element) => {cy.get(element).click({force: true})}) 
+
+Cypress.Commands.add('optionalText', (element) => {cy.get(element).click({force: true})}) 
