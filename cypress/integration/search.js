1dccc0f19345a522e3e2896b76b42d22814b2332
ZampaglioneGaston <83976674+ZampaglioneGaston@users.noreply.github.com>
2021-05-11 21:34:00 -0300

    Clean version

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -12,27 +12,28 @@ describe('DeliveryPizza', function(){
     })
 
     it('PedidosYa', function(){
-        cy.visit('/');                                                                 // Ingreso a Pedidos Ya.                   
+        //Access
+        cy.visit('/');                                                                                
         cy.World(this.parameter.Country, this.parameter.CityList, this.parameter.CityIndex, 121)  // Cordoba
         cy.get(this.parameter.Adress).type('Duarte Quiros 10')
         cy.get(this.parameter.Option).type('Pizza')
         cy.AcceptMap(this.parameter.SearchB, this.parameter.ConfirmMap);
 
-
-
-                    
+        //Restaurant
         cy.get(this.parameter.Sort).click();                                            // Menu de ordenamiento
-
         cy.sort('[value="rating"] > a');                                                // Ordeno por puntuacion
         cy.restaurant('[data-url="https://www.pedidosya.com.ar/restaurantes/cordoba/grido-helados-3122-velez-sarsfield-ii-av-velez-sarsfield-272-menu"] > .restaurantData > .infoFooter > .callToAction > .button');
 
+        //Item
         cy.item('[data-id="23570597"] > b');                                            // Item > pizza
         cy.wait(1000);
 
+        //Delivery
         cy.get(this.parameter.OptionalText).type('Que venga caliente pls')              // Opcional en pedido
         cy.wait(1000);
         cy.get(this.parameter.AddDelivery).click();                                     // Boton agregar al pedido
 
+        //Direction
         cy.get(this.parameter.Adress).type('Duarte Quiros 10', {force: true})  
         cy.AcceptMap(this.parameter.SearchB, this.parameter.ConfirmMap); 
 
