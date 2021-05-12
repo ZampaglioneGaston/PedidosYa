c5d277a9cdb13b9f92c2416a1e44df48f0cd7d5f
ZampaglioneGaston <83976674+ZampaglioneGaston@users.noreply.github.com>
2021-05-11 21:29:41 -0300

    Clear version

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -23,18 +23,15 @@ describe('DeliveryPizza', function(){
                     
         cy.get(this.parameter.Sort).click();                                            // Menu de ordenamiento
 
-        cy.sort('[value="rating"] > a');                                               // Ordeno por puntuacion
+        cy.sort('[value="rating"] > a');                                                // Ordeno por puntuacion
         cy.restaurant('[data-url="https://www.pedidosya.com.ar/restaurantes/cordoba/grido-helados-3122-velez-sarsfield-ii-av-velez-sarsfield-272-menu"] > .restaurantData > .infoFooter > .callToAction > .button');
 
-        cy.item('[data-id="23570597"] > b');                                           // Item > pizza
+        cy.item('[data-id="23570597"] > b');                                            // Item > pizza
         cy.wait(1000);
 
         cy.get(this.parameter.OptionalText).type('Que venga caliente pls')              // Opcional en pedido
         cy.wait(1000);
         cy.get(this.parameter.AddDelivery).click();                                     // Boton agregar al pedido
-
-            
-
 
         cy.get(this.parameter.Adress).type('Duarte Quiros 10', {force: true})  
         cy.AcceptMap(this.parameter.SearchB, this.parameter.ConfirmMap); 
